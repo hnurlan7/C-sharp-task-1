e0fc6185d57272aeef1ee5ace8993116247f2890
hnurlan7 <tu6f7xcr3@code.edu.az>
2022-03-03 22:09:49 +0400

    Add project files.

---
+++ C-sharp-tasks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C_sharp_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter month: ");
+            string month = Console.ReadLine();
+            Console.WriteLine("Season: ");
+            if (month == "December" || month == "January" || month == "February")
+            {
+                Console.WriteLine("Winter");
+            }
+            else if (month == "March" || month == "April" || month == "May")
+            {
+                Console.WriteLine("Spring");
+            }
+            else if (month == "June" || month == "July" || month == "August")
+            {
+                Console.WriteLine("Summer");
+            }
+            else if (month == "September" || month == "October" || month == "November")
+            {
+                Console.WriteLine("Autumn");
+            }
+        }
+    }
+}
